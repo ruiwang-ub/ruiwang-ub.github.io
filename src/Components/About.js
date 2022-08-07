c8a7176acc96282604255b81b404ca8a4df7a1dc
Yunfan Xu <xuyunfan17@gmail.com>
2022-08-07 18:33:23 -0400

    update contact function

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,7 @@ class About extends Component {
     const city = this.props.data.address.city;
     const state = this.props.data.address.state;
     const zip = this.props.data.address.zip;
-    const phone = this.props.data.phone;
+    // const phone = this.props.data.phone;
     const email = this.props.data.email;
     const resumeDownload = this.props.data.resumedownload;
 
@@ -43,8 +43,8 @@ class About extends Component {
                       {city} {state}, {zip}
                     </span>
                     <br />
-                    <span>{phone}</span>
-                    <br />
+                    {/* <span>{phone}</span>
+                    <br /> */}
                     <span>{email}</span>
                   </p>
                 </div>
